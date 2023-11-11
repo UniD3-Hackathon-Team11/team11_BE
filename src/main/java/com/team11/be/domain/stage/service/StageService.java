73611e9926016c97243d25614bf36dd0ae193c24
KarmaPol <kkhkr98@gmail.com>
2023-11-12 00:35:30 +0900

    feat : stage controller, service 코드 추가

--- src/main/java/com/team11/be/domain/stage/service/StageService.java
+++ src/main/java/com/team11/be/domain/stage/service/StageService.java
@@ -6,17 +6,24 @@ import com.team11.be.domain.stage.model.Stage;
 import com.team11.be.domain.stage.repository.StageRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class StageService {
 
     private final StageRepository stageRepository;
 
     public Stage getStage(Long id){
         return stageRepository.findById(id).orElseThrow(() -> new RuntimeException("아직 없는 게임이에요"));
+    }
+
+    public StageResponseDto getStageResponse(Long id){
+        Stage stage = getStage(id);
+        return new StageResponseDto(stage.getId(), stage.getTargetScore(), stage.getTimeLimit());
     }
 
     public void postStage(StageRequestDto stageRequestDto){
@@ -29,5 +36,10 @@ public class StageService {
     public List<StageResponseDto> getAllStages(){
         List<Stage> stages = stageRepository.findAll();
         return stages.stream().map(s -> new StageResponseDto(s.getId(), s.getTargetScore(), s.getTimeLimit())).toList();
+    }
+
+    public void deleteStage(Long id){
+        Stage stage = getStage(id);
+        stageRepository.delete(stage);
     }
 }
