540aa4be43c2e4ffd3e4f3c968d1dd5e716d827e
KarmaPol <kkhkr98@gmail.com>
2023-11-12 00:01:25 +0900

    feat : add Stage model

---
+++ src/main/java/com/team11/be/domain/stage/model/Stage.java
@@ -0,0 +1,23 @@
+package com.team11.be.domain.stage.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Stage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @ColumnDefault("5")
+    private Integer targetScore;
+    @ColumnDefault("300")
+    private Integer timeLimit = 300;
+}
