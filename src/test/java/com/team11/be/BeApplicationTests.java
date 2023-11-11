8b9c61cd96a79660fb36b455a8d493bcd1b663d3
KarmaPol <kkhkr98@gmail.com>
2023-11-12 01:39:50 +0900

    build : 빌드 파일 추가

--- src/test/java/com/team11/be/BeApplicationTests.java
+++ src/test/java/com/team11/be/BeApplicationTests.java
@@ -1,13 +1,10 @@
 package com.team11.be;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class BeApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//@SpringBootTest
+//class BeApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
