3484f88b41c703effe3366e7300899c6f548eb0b
KarmaPol <kkhkr98@gmail.com>
2023-11-11 21:04:41 +0900

    feat : add cors config ;

---
+++ src/main/java/com/team11/be/global/exception/config/CorsConfig.java
@@ -0,0 +1,17 @@
+package com.team11.be.global.exception.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowCredentials(true) // 쿠키 정책 허용
+                .maxAge(3600);
+    }
+}
