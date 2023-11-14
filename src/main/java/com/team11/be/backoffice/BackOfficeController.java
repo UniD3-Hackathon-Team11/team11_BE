e7b6f4e2648bf03ee07d9fa85fba87455fe40665
KarmaPol <kkhkr98@gmail.com>
2023-11-14 12:07:28 +0900

    feat : qr 추가, readme 수정

--- src/main/java/com/team11/be/backoffice/BackOfficeController.java
+++ src/main/java/com/team11/be/backoffice/BackOfficeController.java
@@ -1,5 +1,10 @@
 package com.team11.be.backoffice;
 
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
 import com.team11.be.domain.stage.dto.StageRequestDto;
 import com.team11.be.domain.stage.dto.StageResponseDto;
 import com.team11.be.domain.stage.service.StageService;
@@ -10,6 +15,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.Base64;
 import java.util.List;
 
 @Controller
@@ -43,5 +51,26 @@ public class BackOfficeController {
         stageService.deleteStage(id);
 
         return "redirect:/backoffice/stages";
+    }
+
+    @GetMapping("/backoffice/stages/getQrcode/{id}")
+    public String getQrcode(Model model, @PathVariable Long id) throws Exception {
+
+        String img = getQRCodeImage(String.valueOf(id), 200, 200);
+        model.addAttribute("img", img);
+
+        return "stages/qrcode";
+    }
+
+    // QR코드 이미지 생성
+    public static String getQRCodeImage(String text, int width, int height) throws WriterException, IOException {
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+        BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, width, height);
+
+        ByteArrayOutputStream pngOutputStream = new ByteArrayOutputStream();
+
+        MatrixToImageWriter.writeToStream(bitMatrix, "PNG", pngOutputStream);
+
+        return Base64.getEncoder().encodeToString(pngOutputStream.toByteArray());
     }
 }
